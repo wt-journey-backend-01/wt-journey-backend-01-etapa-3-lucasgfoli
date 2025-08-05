007c0b74ed4a5fe5c0d78f6c79ea40ae7aa785c7
Lucas Oliveira <Lucas.Oliveira@geothra.com>
2025-08-05 08:57:56 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,27 +3,37 @@ require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-
-const isCI = process.env.NODE_ENV === 'ci' || process.env.GITHUB_ACTIONS === 'true';
-
-const baseConfig = {
-  client: 'pg',
-  connection: {
-    host: isCI ? '127.0.0.1' : 'postgres',
-    port: 5432,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  },
-  migrations: {
-    directory: './db/migrations',
-  },
-  seeds: {
-    directory: './db/seeds',
-  },
-};
-
 module.exports = {
-  development: baseConfig,
-  ci: baseConfig,
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'postgres',
+      port: 5432,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+  ci: {
+    client: 'pg',
+    connection: {
+      host: 'postgres', 
+      port: 5432,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
 };
