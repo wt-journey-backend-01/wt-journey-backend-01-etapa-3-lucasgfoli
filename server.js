26dcdef251b6fcc7581a7cf53f210c60f304045a
lucasgabrielfer.oli@gmail.com <lucasgabrielfer.oli@gmail.com>
2025-08-06 17:25:36 -0300

    adicionando swagger.js

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express()
 const PORT = 3000
 const agentesRoutes = require('./routes/agentesRoutes')
 const casosRoutes = require('./routes/casosRoutes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require('./docs/swagger.js')
 
 app.use(express.json())
 app.use('/agentes', agentesRoutes)
 app.use('/casos', casosRoutes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, ()=> {
     console.log(`🚀Servidor rodando na porta ${PORT}`)
