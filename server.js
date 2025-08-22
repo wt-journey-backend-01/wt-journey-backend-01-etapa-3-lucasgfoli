54cf660a3109084c7b8d4013673daa6682175824
lucasgabrielfer.oli@gmail.com <lucasgabrielfer.oli@gmail.com>
2025-08-22 19:00:16 -0300

    add files

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-const agentesRouter = require('./routes/agentesRouter.js')
-const casosRouter = require('./routes/casosRouter.js')
+const agentesRoutes = require('./routes/agentesRoutes.js')
+const casosRoutes = require('./routes/casosRoutes.js')
 const swaggerUi = require('swagger-ui-express')
 const swaggerSpec = require('./docs/swagger.js')
 
 app.use(express.json())
-app.use('/agentes', agentesRouter)
-app.use('/casos', casosRouter)
+app.use('/agentes', agentesRoutes)
+app.use('/casos', casosRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, '0.0.0.0', ()=> {
